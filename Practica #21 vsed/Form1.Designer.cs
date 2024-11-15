adfe75828765b6bc1005a831266e2e14bcbd8178
USER <USER@DIAMAND>
2024-11-14 20:25:38 -0600

    Agregar archivos de proyecto.

---
+++ Practica #21 vsed/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Practica__21_vsed
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Cooper Black", 14.25F);
+            button1.Location = new Point(472, 144);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 61);
+            button1.TabIndex = 0;
+            button1.Text = "listar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Cooper Black", 14.25F);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(188, 93);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(166, 319);
+            listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cooper Black", 14.25F);
+            label1.Location = new Point(239, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(315, 21);
+            label1.TabIndex = 2;
+            label1.Text = "\"escribe los numeros del 0 al 10\"";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Label label1;
+    }
+}
